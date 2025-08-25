f943dcd39fb6bab96cf3c62bcb4da71e083ba7a9
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-08-25 11:53:53 +0200

    add y/n confirmation

--- conda_self/cli/main_migrate.py
+++ conda_self/cli/main_migrate.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from conda.reporters import confirm_yn
+
 
 if TYPE_CHECKING:
     import argparse
@@ -9,7 +11,7 @@ HELP = "Migrate your `base` environment into a new  environment named `default` 
 WHAT_TO_EXPECT ="""
     This will:
 
-    1. Duplicate your <env name> environment in a new environment named `default`.
+    1. Duplicate your base environment in a new environment named `default`.
     2. Reset the `base` environment to only the essential packages and plugins.
     3. Protect the `base` environment, which prevents you from making further changes to it 
        (this behavior can be overriden using the `--override-frozen` flag).
@@ -19,10 +21,7 @@ WHAT_TO_EXPECT ="""
 
     1. Accidental breakage of the conda installation    
     2. Bloated and complex environments
-
-    Proceed with migrating your <env name> environment?[y/n]:
     """
-
 
 def configure_parser(parser: argparse.ArgumentParser) -> None:
     parser.description = HELP
@@ -34,6 +33,9 @@ def execute(args: argparse.Namespace) -> int:
     from ..reset import reset
 
     print(WHAT_TO_EXPECT)
+
+    confirm_yn("Proceed with migrating your base environment?[y/n]:\n", default="no", dry_run=False)
+
     print("Resetting 'base' environment...")
     uninstallable_packages = permanent_dependencies()
     reset(uninstallable_packages=uninstallable_packages)
