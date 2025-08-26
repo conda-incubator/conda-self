6444979083b42cad710c108565090def65523ede
Mahe Iram Khan <forgotten.programme@gmail.com>
2025-08-26 13:37:15 +0200

    add help messages to the output, add confirm_yn

--- conda_self/cli/main_protect.py
+++ conda_self/cli/main_protect.py
@@ -1,13 +1,47 @@
 from __future__ import annotations
 
-import sys
 from typing import TYPE_CHECKING
+from conda.reporters import confirm_yn
+import sys
+
 
 if TYPE_CHECKING:
     import argparse
 
-HELP = "Protect 'base' environment from any further modifications"
+HELP = "Migrate your `base` environment into a new  environment named `default` and protect your base."
+WHAT_TO_EXPECT ="""
+    This will:
 
+    1. Duplicate your base environment to a new environment named `{env_name}`.
+    2. Reset the `base` environment to only the essential packages and plugins.
+    3. Protect the `base` environment, which prevents you from making further changes to it 
+       (this behavior can be overriden using the `--override-frozen` flag).
+    4. Activate your duplicate environment.
+
+    This helps prevent issues like:
+
+    1. Accidental breakage of the conda installation    
+    2. Bloated and complex environments
+    """
+
+SUCCESS_MESSAGE=""" 
+    SUCCESS! 
+    The following operations were completed:
+
+    1. Duplication of your current `base` environment to `{env_name}`.
+    2. Resetting of the `base` to only the essential packages and plugins.
+    3. Protection of the `base` which prevents it from being modified (unless an override flag is used).
+    4. Activation your duplicate environment `{env_name}`.
+"""
+
+BEST_PRACTICES="""
+    BEST PRACTICES
+    Follow these tips for a smoother `conda` experience:
+
+    1. Do not modify the `base` environment.
+    2. Use a different environment for your work going forward.
+    3. Create a new environment for each new project.
+"""
 
 def configure_parser(parser: argparse.ArgumentParser) -> None:
     parser.description = HELP
@@ -34,6 +68,10 @@ def execute(args: argparse.Namespace) -> int:
 
     from ..query import permanent_dependencies
     from ..reset import reset
+
+    print(WHAT_TO_EXPECT.format(env_name=args.default_env))
+
+    confirm_yn("Proceed with migrating your base environment?[y/n]:\n", default="no", dry_run=False)
 
     print("Protecting 'base' environment...")
     uninstallable_packages = permanent_dependencies()
@@ -88,4 +126,6 @@ def execute(args: argparse.Namespace) -> int:
     rc_config["default_activation_env"] = str(dest_prefix_data.prefix_path)
     _write_rc(sys_rc_path, rc_config)
 
+    print(SUCCESS_MESSAGE.format(env_name=args.default_env))
+    print(BEST_PRACTICES)
     return 0
