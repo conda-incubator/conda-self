d30119ea659cd6754a64b6e87643abba271496a5
jaimergp <jaimergp@users.noreply.github.com>
2025-09-14 18:08:24 +0200

    fix and merge tests

--- tests/test_cli_reset.py
+++ tests/test_cli_reset.py
@@ -1,9 +1,6 @@
-import subprocess
-import sys
-from pathlib import Path
-
-from conda.core.prefix_data import PrefixData
 from conda.testing.fixtures import TmpEnvFixture
+
+from conda_self.testing import conda_cli_subprocess, is_installed
 
 
 def test_help(conda_cli):
@@ -13,42 +10,15 @@ def test_help(conda_cli):
 
 def test_reset(conda_cli, tmp_env: TmpEnvFixture):
     with tmp_env("conda", "conda-self") as prefix:
-        if Path(prefix, "conda-meta", "frozen").exists():
-            Path(prefix, "conda-meta", "frozen").unlink()
+        assert not is_installed(prefix, "numpy")
 
-        assert len(tuple(PrefixData(prefix).query("numpy"))) == 0
+        conda_cli("install", "numpy", "--yes", "--prefix", prefix)
+        assert is_installed(prefix, "numpy")
 
-        conda_cli("install", "numpy", "--yes")
-
-        assert len(tuple(PrefixData(prefix).query("numpy"))) == 1
-
-        conda_cli(
-            "self",
-            "reset",
-            "--yes",
-        )
-        assert len(tuple(PrefixData(prefix).query("numpy"))) == 0
-
-
-def test_reset_conda_self_present(tmp_env: TmpEnvFixture):
-    with tmp_env("conda", "conda-self") as prefix:
-        # platform is "win32" even in win64 machines
-        if sys.platform == "win32":
-            python_bin = prefix / "python.exe"
-        else:
-            python_bin = prefix / "bin" / "python"
-
-        PrefixData._cache_.clear()
-        # Note: Running python -m conda self incidentally loads 'conda_self' from
-        # the repo because the working directory happens to contain it. We should
-        # actually 'pip install .' the repo but in this case we are lucky and don't
-        # need to.
-        subprocess.run(
-            [str(python_bin), "-m", "conda", "self", "reset", "--yes"],
-            check=True,
-        )
-
+        conda_cli_subprocess(prefix, "self", "reset", "--yes")
         # make sure conda-self didn't remove conda
-        assert PrefixData(prefix).get("conda")
+        assert is_installed(prefix, "conda")
         # make sure conda-self didn't remove itself
-        assert PrefixData(prefix).get("conda-self")
+        assert is_installed(prefix, "conda-self")
+        # but numpy should be gone
+        assert not is_installed(prefix, "numpy")
