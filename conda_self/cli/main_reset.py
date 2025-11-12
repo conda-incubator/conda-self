237198835b9df56cf259affce89517dd986661b2
Marco Esters <mesters@anaconda.com>
2025-11-12 14:22:25 -0800

    Provide CLI to reset to installer and migration lock file

--- conda_self/cli/main_reset.py
+++ conda_self/cli/main_reset.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import sys
+from pathlib import Path
 from textwrap import dedent
 from typing import TYPE_CHECKING
 
@@ -7,6 +9,17 @@ if TYPE_CHECKING:
     import argparse
 
 HELP = "Reset 'base' environment to essential packages only."
+RESET_TO_HELP = dedent(
+    """
+    State to reset the `base` environment to.\n
+    `current` removes all packages except for `conda`, its plugins,
+    and their dependencies.
+    `installer` resets the `base` environment to the state provided
+    by the installer.
+    `migrate` resets the `base` environment to state after the last
+    `conda self migrate` command.
+    """
+).lstrip()
 
 WHAT_TO_EXPECT = dedent(
     """
@@ -20,6 +33,15 @@ SUCCESS = dedent(
     Reset the `base` environment to only the essential packages and plugins.
     """
 ).lstrip()
+SUCCESS_STATE = dedent(
+    """
+    SUCCESS!
+    Reset the `base` environment to pre-{state} state.
+    """
+).lstrip()
+
+RESET_FILE_INSTALLER = "initial-state.explicit.txt"
+RESET_FILE_MIGRATE = "migrate-state.explicit.txt"
 
 
 def configure_parser(parser: argparse.ArgumentParser) -> None:
@@ -27,6 +49,12 @@ def configure_parser(parser: argparse.ArgumentParser) -> None:
 
     parser.description = HELP
     add_output_and_prompt_options(parser)
+    parser.add_argument(
+        "--reset-to",
+        choices=("current", "installer", "migrate"),
+        default="current",
+        help=RESET_TO_HELP,
+    )
     parser.set_defaults(func=execute)
 
 
@@ -40,6 +68,19 @@ def execute(args: argparse.Namespace) -> int:
     if not context.quiet:
         print(WHAT_TO_EXPECT)
 
+    if args.reset_to == "installer":
+        reset_file = Path(sys.prefix, "conda-meta", RESET_FILE_INSTALLER)
+    elif args.reset_to == "migrate":
+        reset_file = Path(sys.prefix, "conda-meta", RESET_FILE_MIGRATE)
+    else:
+        reset_file = None
+
+    if reset_file and not reset_file.exists():
+        raise FileNotFoundError(
+            f"Failed to reset to `{args.reset_to}`. "
+            f"Required file {reset_file} not found."
+        )
+
     confirm_yn(
         "Proceed with resetting your 'base' environment?[y/n]:\n",
         default="no",
@@ -48,10 +89,15 @@ def execute(args: argparse.Namespace) -> int:
 
     if not context.quiet:
         print("Resetting 'base' environment...")
-    uninstallable_packages = permanent_dependencies()
-    reset(uninstallable_packages=uninstallable_packages)
+    uninstallable_packages = (
+        permanent_dependencies() if args.reset_to == "current" else set()
+    )
+    reset(uninstallable_packages=uninstallable_packages, reset_to=reset_file)
 
     if not context.quiet:
-        print(SUCCESS)
+        if args.reset_to == "current":
+            print(SUCCESS)
+        else:
+            print(SUCCESS_STATE.format(state=args.reset_to))
 
     return 0
