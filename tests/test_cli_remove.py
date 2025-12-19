ae8bee1df58d6f36791ea27268554fa000e29f41
Marco Esters <mesters@anaconda.com>
2025-12-19 08:10:58 -0800

    Add test to remove non-essential plug-in

--- tests/test_cli_remove.py
+++ tests/test_cli_remove.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import pytest
 
 from conda_self.exceptions import SpecsCanNotBeRemoved
+from conda_self.testing import conda_cli_subprocess, is_installed
+
+if TYPE_CHECKING:
+    from conda.testing.fixtures import TmpEnvFixture
+    from pytest import MonkeyPatch
 
 
 def test_help(conda_cli):
@@ -23,3 +32,25 @@ def test_remove_plugin(conda_cli, spec, error):
         spec,
         raises=error,
     )
+
+
+def test_remove_nonessential_plugin(
+    monkeypatch: MonkeyPatch,
+    tmp_env: TmpEnvFixture,
+    conda_channel: str,
+    python_version: str,
+):
+    monkeypatch.setenv("CONDA_CHANNELS", conda_channel)
+
+    # Adding conda-index as a non-essential plug-in
+    with tmp_env(
+        "conda", "conda-self", f"python={python_version}", "conda-index"
+    ) as prefix:
+        assert is_installed(prefix, "conda-index")
+        conda_cli_subprocess(
+            prefix,
+            "self",
+            "remove",
+            "conda-index",
+        )
+        assert not is_installed(prefix, "conda-index")
